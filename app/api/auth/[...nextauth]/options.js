93a1747a7b422015c1b379bfe68b7b696b59bd8f
mat_dweb <mat.dweb28@gmail.com>
2023-08-21 09:05:17 -0600

    setup options for authProviders

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -4,7 +4,7 @@ export default {
     providers: [
         SpotifyProvider({
             authorization:
-                'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private',
+                'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,playlist-modify-private,playlist-modify-public',
             clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
             clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
         }),
