157c8fba38ba3b31aa9f2510bb68d4e19e2cc1af
mat_dweb <mat.dweb28@gmail.com>
2023-08-26 09:45:44 -0600

    display correctly the new_playlist information

--- app/components/NewPlaylistMessage.js
+++ app/components/NewPlaylistMessage.js
@@ -5,11 +5,11 @@ function NewPlaylistMessage({ content }) {
         <div>
             <div className='ml-10'>
                 <h1 className='primary-title'>New Playlist created</h1>
-                <div className='flex flex-col flex-nowrap justify-start items-start text-3xl pb-6'>
+                <div className='flex flex-col flex-nowrap justify-start items-start text-3xl pb-8'>
                     <label>Name</label>
-                    <p>{content.playlist?.name}</p> <br />
+                    <p>{content.new_playlist?.name}</p> <br />
                     <label>Description</label>
-                    <p>{content.playlist?.description}</p>
+                    <p>{content.new_playlist?.description}</p>
                 </div>
             </div>
             <span className='gray-txt'>See Songs</span>
