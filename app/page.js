01a8e9442554d5d62e8d06f132755491f6035d7b
mat_dweb <mat.dweb28@gmail.com>
2023-08-26 12:19:21 -0600

    create a modal component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { useSession, signIn, signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { useState } from 'react';
 import NotSignedIn from './components/NotSignedIn';
 import SignedIn from './components/SignedIn';
